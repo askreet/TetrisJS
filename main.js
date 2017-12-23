0c17043646d62b898dd35dee42afc960b1ec4aa0
Kyle Smith <askreet@gmail.com>
2017-12-23 15:24:55 -0500

    playing with aliases.

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-let app = new PIXI.Application({ width: 800, height: 600 });
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
+let app = new Application({ width: 800, height: 600 });
+
 
 document.body.appendChild(app.view);
 
-PIXI.loader
-    .add("images/block16.png")
+loader
+    .add("block16", "images/block16.png")
     .load(setup);
 
 function setup() {
-    let blockSprite = new PIXI.Sprite(PIXI.loader.resources["images/block16.png"].texture);
+    let blockSprite = new Sprite(resources.block16.texture);
 
     app.stage.addChild(blockSprite);
 }
