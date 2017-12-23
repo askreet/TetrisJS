f207773e23c4e9f9dc0a699638d835cca9aa2361
Kyle Smith <askreet@gmail.com>
2017-12-23 15:18:20 -0500

    added sprite

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 let app = new PIXI.Application({ width: 800, height: 600 });
 
 document.body.appendChild(app.view);
+
+PIXI.loader
+    .add("images/block16.png")
+    .load(setup)
+
+function setup() {
+    let blockSprite = PIXI.Sprite(PIXI.loader.resources["images/block16.png"].texture);
+
+    alert("Loading complete!");
+
+    app.stage.addChild(blockSprite);
+}
