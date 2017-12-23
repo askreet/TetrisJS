9131dce1447cb13b16476eb9603073b3128778fd
Kyle Smith <askreet@gmail.com>
2017-12-23 15:19:15 -0500

    missing semicolon

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.body.appendChild(app.view);
 
 PIXI.loader
     .add("images/block16.png")
-    .load(setup)
+    .load(setup);
 
 function setup() {
     let blockSprite = PIXI.Sprite(PIXI.loader.resources["images/block16.png"].texture);
