72b8c25ab83f54c1f07fefdecba5c813caf6b194
Kyle Smith <askreet@gmail.com>
2017-12-23 20:58:27 -0500

    board absorbs ghosts

--- src/board.js
+++ src/board.js
@@ -74,8 +74,8 @@ class Board {
     }
 
     * everyLocation() {
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
+        for (let x = 1; x < this.width; x++) {
+            for (let y = 1; y < this.height; y++) {
                 yield new Location(x, y);
             }
         }
